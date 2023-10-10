f4f12f6d2ff46e5f5f41523b60d0d89c470aa5f9
maki <141129686+kawasakimaki@users.noreply.github.com>
2023-10-10 17:59:30 +0000

    ルーティングの設定/footerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+
+  scope module: :public do
+  root to: 'homes#top'
   end
-  devise_for :admins
-  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
