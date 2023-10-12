6b6c0fa4b2fa9bf893bacb31d69455e8a6a8eeb5
kawasakimaki <kchurip.08@gmail.com>
2023-10-12 17:59:48 +0000

    controllerの記述を追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,17 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-    @items = Iuems.all
+    @items = Items.all
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:id, :customer_id, :category_id, :name, :description, :price, :area, :store)
   end
 
 end
