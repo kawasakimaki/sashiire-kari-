e7ca993f34ce2a9310dbd4e07f81aed5abaa8dac
kawasakimaki <kchurip.08@gmail.com>
2023-10-15 20:40:17 +0000

    商品投稿時の動作確認

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,14 @@
 class Public::ItemsController < ApplicationController
 
-  def create
+  def new
     @item = Item.new
+    @categories = Category.all
+  end
+
+  def create
+    @item = Item.new(item_params)
     @item.save
-    redirect_to item_path(@item.id)
+    redirect_to item_path(@item)
   end
 
   def index
@@ -13,6 +18,16 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path(item.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :customer
-  belongs_to :category
+  # belongs_to :category
   has_many :like_lists, dependent: :destroy
   has_many :comment_lists, dependent: :destroy
 
