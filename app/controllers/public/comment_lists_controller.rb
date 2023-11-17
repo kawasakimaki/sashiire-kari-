e092ace52ac6d0025d581cee8b56d82519620c54
maki <kchurip.08@gmail.com>
2023-11-17 15:30:19 +0000

    会員ヘッダーにいいね一覧を追加

--- app/controllers/public/comment_lists_controller.rb
+++ app/controllers/public/comment_lists_controller.rb
@@ -1,16 +1,16 @@
 class Public::CommentListsController < ApplicationController
 
   def create
-    item = Item.find(params[:item_id])
-    comment = current_customer.comment_lists.new(comment_list_params)
-    comment.item_id = item.id
-    comment.save
-    redirect_to item_path(item)
+    @item = Item.find(params[:item_id])
+    @comment = current_customer.comment_lists.new(comment_list_params)
+    @comment.item_id = @item.id
+    @comment.save
   end
 
   def destroy
-    CommentList.find(params[:id]).destroy
-    redirect_to item_path(params[:item_id])
+    @comment = CommentList.find(params[:id])
+    @item = @comment.item
+    @comment.destroy
   end
 
   private
