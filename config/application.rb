0d71a5004f62df1a2319ff707ce6ce604c548f41
maki <kchurip.08@gmail.com>
2023-11-26 14:26:36 +0000

    タイムゾーンの設定を東京に変更/商品詳細画面に時刻を追加

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,11 @@ module Sashiire
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Tokyo'
+    #  DBに保存する時刻も日本時間に設定
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
